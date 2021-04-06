6f84d20569e15fae7c67981a21021222ccbd5af5
Zixuan Zhang <zixuanzhang.x@gmail.com>
2021-04-06 17:39:21 -0500

    Setup firebase, routing, and Vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { firestorePlugin } from "vuefire"; // Added to make vuefire work
+import { auth } from "./firebaseConfig.js"; // for authentication
 
-Vue.config.productionTip = false
+import "@/assets/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false;
+Vue.use(firestorePlugin); // added to make vuefire work
+
+let app; // for authentication
+auth.onAuthStateChanged(() => {
+  // for authentication
+  if (!app) {
+    // for authentication
+    app = new Vue({
+      router,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
